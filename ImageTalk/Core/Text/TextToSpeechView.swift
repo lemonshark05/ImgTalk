10987b6a1393c4bb9a93b93ec3ee9ed7f5dbbd26
zhimin meng <mengzhimin5@gmail.com>
2023-10-12 15:41:08 -0700

    Add a button

---
+++ ImageTalk/Core/Text/TextToSpeechView.swift
@@ -0,0 +1,18 @@
+//
+//  TextToSpeechView.swift
+//  ImageTalk
+//
+//  Created by lemonshark on 2023/10/11.
+//
+
+import SwiftUI
+
+struct TextToSpeechView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextToSpeechView()
+}
